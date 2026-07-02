870dcd2d5742c433c8d5c772d887845e3d081ee5
aslion151-boop <aslion151@gmail.com>
2026-07-02 23:53:03 +0200

    Refresh bundle loader cache keys

--- frontend/frontend-skin/app.bundle.js
+++ frontend/frontend-skin/app.bundle.js
@@ -1,6 +1,5 @@
-/* Ernos Zdravstvena Njega loader
-   Keeps the original UI bundle intact and loads the Croatian home-care cleanup after it. */
+/* Ernos Zdravstvena Njega loader */
 (function(){
-  document.write('<script src="/app.bundle.legacy.js?v=9"><\\/script>');
-  document.write('<script src="/hr-rebrand.js?v=20260702-4"><\\/script>');
+  document.write('<scr'+'ipt src="/app.bundle.legacy.js?v=20260702-6"><\\/scr'+'ipt>');
+  document.write('<scr'+'ipt src="/hr-rebrand.js?v=20260702-6"><\\/scr'+'ipt>');
 })();
